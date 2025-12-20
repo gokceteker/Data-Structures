0107fe718f8256f1e1a3439d7fb4443dd5bb7c64
Gökçe Teker <gokceteker@hotmail.com>
2025-12-20 16:36:52 +0300

    week6_tree_preorder.c

---
+++ Week06_BinaryTree/week6_tree_preorder.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+void preOrder(struct Node* root) {
+    if (root == NULL) return;
+
+    printf("%d ", root->data);
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+int main() {
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+    root->right->left = createNode(6);
+    root->right->right = createNode(7);
+
+    printf("Pre-order: ");
+    preOrder(root);
+    printf("\n");
+
+    return 0;
+}
